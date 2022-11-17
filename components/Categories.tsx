85c1367c8c45e3d7cd6f51b4249fd3970e203d6c
Rich Leach <rich@cfsnap.com>
2022-11-17 13:17:38 -0700

    no message

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -16,6 +16,8 @@ const Categories = () => {
     <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
       <h3 className="text-xl mb-2 font-semibold border-b pb-4">Blog Categories</h3>
       {categories.map((category, index) => (
+        
+      // @ts-ignore
         <Link key={index} href={`/blog/${category.slug}`}>
           <span className={`cursor-pointer block ${(index === categories.length - 1) ? 'border-b-0' : 'border-b'} pb-1 mb-3`}>{category.name}</span>
         </Link>
