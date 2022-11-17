e0c63c16feefbef4b0270e1e769938140902fde7
Rich Leach <rich@cfsnap.com>
2022-11-17 13:04:33 -0700

    changed scripting per Vercel docs

--- next.config.js
+++ next.config.js
@@ -2,7 +2,9 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-    ignoreBuildErrors: true
+    typescript: {
+        ignoreBuildErrors: true
+    }
   }
   
   module.exports = nextConfig
